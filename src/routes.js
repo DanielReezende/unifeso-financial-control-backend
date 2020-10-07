db0348d7a7b41ba8957b506ed4e69d745eb23ae7
Daniel Rezende <daniel.zende@gmail.com>
2020-10-07 20:10:04 -0300

    Criada todas as rotas do usuario

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,12 @@ const express = require('express')
 const routes = express.Router()
 const { UserController } = require('./controllers')
 
+routes.get('/', UserController.index)
+routes.get('/:id', UserController.show)
+routes.put('/:id', UserController.update)
+routes.delete('/:id', UserController.destroy)
 routes.post('/', UserController.store)
 
-// Route params /1
-// Query params /user?nome=daniel
-
-// Post = Criar
-// Get = Para pegar tudo ou um especifico
-// put = Atualizar
-// delete = deletar
 
 
 module.exports = routes;
